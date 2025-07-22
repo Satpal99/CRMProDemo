145d7db6ab331cf8edd2dfa3e9cc05378c6ab6f7
Satpal99 <satpal110099@gmail.com>
2025-07-22 17:00:01 +0530

    this is genuine

--- src/test/java/demo/Sp1Branch.java
+++ src/test/java/demo/Sp1Branch.java
@@ -2,6 +2,6 @@ package demo;
 
 public class Sp1Branch {
 public static void main(String[] args) {
-	System.out.println("this is master branch");
+	System.out.println("this is master branch not issue");
 }
 }
